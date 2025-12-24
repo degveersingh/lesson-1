225b48bf2e0f7119c0a98c38a3672c7021069559
degveersingh <degveersingh@gmail.com>
2025-12-24 12:34:16 +0400

    lesson 1

--- activity1.py
+++ activity1.py
@@ -1,1 +1,11 @@
 print("hello welcome to codingal")
+print(6)
+
+#next line after print
+print("hello python\n")
+
+#pass multiple arguement for printing
+print("hello",5)
+
+#end arguement of print statement
+print("welcome to ",end="^")
---
+++ activity2.py
@@ -0,0 +1,9 @@
+h=7
+d="degveer"
+print(h,d)
+f=6
+l="l"
+print(f,l)
+
+name=input("enter your name:")
+print("\nhello",name,"\nwelcome to codingal")
