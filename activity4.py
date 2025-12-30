aa3733307f1ef4dc6c8e40255a6353609b4cd8fd
degveersingh <degveersingh@gmail.com>
2025-12-30 19:16:37 +0400

    lesson1

---
+++ activity4.py
@@ -0,0 +1,12 @@
+birthday1 = "12-03-2001"
+birthday2 = "25-07-2000"
+birthday3 = "09-11-1999"
+birthday4 = "18-01-2002"
+birthday5 = "30-06-2003"
+
+# Displaying the birthdays
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
